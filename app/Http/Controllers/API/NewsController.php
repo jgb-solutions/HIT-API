08240a5bb00e45a488716aee25ec1ed9097a3139
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2020-03-26 01:20:01 -0400

    Ordered by ID desc instead

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -12,7 +12,7 @@
     {
       $take = is_numeric($request->get('take')) ? $request->get('take'): 12;
 
-      return News::orderBy('created_at', 'desc')->paginate($take);
+      return News::orderBy('id', 'desc')->paginate($take, ['hash', 'image', 'title', 'date']);
     }
 
     public function show($newsHash)
